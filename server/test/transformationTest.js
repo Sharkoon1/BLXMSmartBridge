4cd1574c4af4146da8b2370a8f2c59c6c3c898e2
Mirco <sharkoon1972@gmail.com>
2021-11-22 19:48:12 +0100

    added transformation service and transformation test

---
+++ server/test/transformationTest.js
@@ -0,0 +1,25 @@
+var assert = require('assert');
+describe('AlgoTest', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+  describe('2#', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+  describe('3#', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+  describe('4#', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+  
+  
+});
