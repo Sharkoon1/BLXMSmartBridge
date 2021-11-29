32d8db32077fbf52c79f84228eac07e9414b5e14
Jan <jan.wardenga@gmail.com>
2021-11-29 18:24:28 +0100

    update flowcharts with comments

--- test/TransformationService.test.js
+++ test/TransformationService.test.js
@@ -11,4 +11,3 @@ const getAdjustmentValue = require("../service/GetAdjustmentValueService.js");
 test("Returns 851.8569696479246 for BLXM_CHEAP = 1310, USD_CHEAP = 5000, BLXM_EXPANSIVE = 233.9606946033066, USD_EXPANSIVE = 19234", () => {
     expect(getAdjustmentValue(BLXM_CHEAP, USD_CHEAP, BLXM_EXPANSIVE, USD_EXPANSIVE)).toBe(851.8569696479246);
 });
-
