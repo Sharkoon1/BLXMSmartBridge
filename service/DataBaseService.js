bd4ce073c88061cd09ecf7a4864115816196bdda
Igor <rowan93@web.de>
2021-11-30 18:11:18 +0100

    Added DataBase class as a service.

---
+++ service/DataBaseService.js
@@ -0,0 +1,69 @@
+const Profit = require("./../models/Profit");
+const mongoose = require("mongoose");
+
+class DataBaseService {
+
+    constructor() {
+        const MONGODB_URL = process.env.MONGODB_URL;
+        mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+            //don't show the log when it is test
+            if (process.env.NODE_ENV !== "test") {
+                console.log("Connected to %s", MONGODB_URL);
+                console.log("App is running ... \n");
+                console.log("Press CTRL + C to stop the process. \n");
+            }
+        })
+            .catch(err => {
+                console.error("App starting error:", err.message);
+                process.exit(1);
+            });
+        this.db = mongoose.connection;
+    }
+
+    QueryData(query) {
+        Profit.find(query)
+            .then(profits => {
+                console.log("success");
+                return profits;
+            })
+            .catch(err => {
+                console.log(err.message);
+            });
+    };
+
+    QueryById(id) {
+        Profit.findById(query)
+            .then(profits => {
+                console.log("success");
+                return profits;
+            })
+            .catch(err => {
+                console.log("Entry " + id + " not found.");
+            });
+    };
+
+    AddData(query) {
+        Profit.create(query)
+            .then(profits => {
+                console.log("success");
+                return profits;
+            })
+            .catch(err => {
+                console.log(err.message);
+            });
+    };
+
+    UpdateById(id, query) {
+        Profit.findByIdAndUpdate(id, query, { new: true })
+            .then(profits => {
+                console.log("success");
+                return profits;
+            })
+            .catch(err => {
+                console.log(err.message);
+            });
+    };
+}
+
+
+module.exports = DataBaseService;
