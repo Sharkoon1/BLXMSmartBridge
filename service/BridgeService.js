a6d4c8cd30b9bf51ab2ae72bb37c18bfb87b5735
Igor <rowan93@web.de>
2021-12-01 17:11:55 +0100

    Added some minor formatting.

--- service/BridgeService.js
+++ service/BridgeService.js
@@ -1,49 +1,82 @@
 const { ethers } = require('ethers');
 const __path = require('path');
-var constants = require('../constants');
-
+var constants = require('../constants.js');
+require("dotenv").config();
+ 
 class BridgeService {
-
+ 
     constructor() {
-        const bridge_abi = require(__path.join(__dirname, '../abi/bridge_abi.json'));
-        const erc20_abi = require(__path.join(__dirname, '../abi/erc20_abi.json'));
-
-        this.wallet_BSC = new ethers.Wallet(process.env.PRIVATE_KEY, constants.PROVIDER_BSC);
-        this.wallet_ETH = new ethers.Wallet(process.env.PRIVATE_KEY, constants.PROVIDER_ETH);
-
-        this.contract_bridge_bsc = new ethers.Contract(constants.BRIDGE_ADDRESS, bridge_abi, this.wallet_BSC);
-        this.contract_bridge_eth = new ethers.Contract(constants.BRIDGE_ADDRESS, bridge_abi, this.wallet_ETH);
-
-        this.contract_blxm_token_bsc = new ethers.Contract(constants.BLXM_TOKEN_ADDRESS_BSC, erc20_abi, this.wallet_BSC);
-        this.contract_usd_token_bsc = new ethers.Contract(constants.USD_TOKEN_ADRESS_BSC, erc20_abi, this.wallet_BSC);
-
-        this.contract_blxm_token_eth = new ethers.Contract(constants.BLXM_TOKEN_ADDRESS_ETH, erc20_abi, this.wallet_ETH);
-        this.contract_usd_token_eth = new ethers.Contract(constants.USD_TOKEN_ADRESS_ETH, erc20_abi, this.wallet_ETH);
+        console.log(process.env.PRIVATE_KEY);
+        this.bridge_abi = require(__path.join(__dirname, '../abi/bridge_abi.json'));
+        this.erc20_abi = require(__path.join(__dirname, '../abi/erc20_abi.json'));
+ 
+        let provider_eth = new ethers.providers.JsonRpcProvider(constants.PROVIDER_ETH);
+        let provider_bsc = new ethers.providers.JsonRpcProvider(constants.PROVIDER_BSC);
+ 
+        this.wallet_BSC = new ethers.Wallet(process.env.PRIVATE_KEY, provider_eth);
+        this.wallet_ETH = new ethers.Wallet(process.env.PRIVATE_KEY, provider_bsc);
+ 
+        this.bridge_wallet_ETH = new ethers.Wallet(process.env.PRIVATE_KEY_BRIDGE, provider_eth);
+        this.bridge_wallet_BSC = new ethers.Wallet(process.env.PRIVATE_KEY_BRIDGE, provider_bsc);
+ 
+        this.contract_blxm_token_bsc = new ethers.Contract(constants.BLXM_TOKEN_ADDRESS_BSC, this.erc20_abi, this.wallet_BSC);
+        this.contract_usd_token_bsc = new ethers.Contract(constants.USD_TOKEN_ADRESS_BSC, this.erc20_abi, this.wallet_BSC);
+ 
+        this.contract_blxm_token_eth = new ethers.Contract(constants.BLXM_TOKEN_ADDRESS_ETH, this.erc20_abi, this.wallet_ETH);
+        this.contract_usd_token_eth = new ethers.Contract(constants.USD_TOKEN_ADRESS_ETH, this.erc20_abi, this.wallet_ETH); 
+ 
+        this.bridge_contract_blxm_token_bsc = new ethers.Contract(constants.BLXM_TOKEN_ADDRESS_BSC, this.erc20_abi, this.bridge_wallet_BSC);
+        this.bridge_contract_usd_token_bsc = new ethers.Contract(constants.USD_TOKEN_ADRESS_BSC, this.erc20_abi, this.bridge_wallet_BSC);
+ 
+        this.bridge_contract_blxm_token_eth = new ethers.Contract(constants.BLXM_TOKEN_ADDRESS_ETH, this.erc20_abi, this.bridge_wallet_ETH);
+        this.bridge_contract_usd_token_eth = new ethers.Contract(constants.USD_TOKEN_ADRESS_ETH, this.erc20_abi, this.bridge_wallet_ETH); 
     }
-
-    async swapBLXMTokenETH() {
-        var contract = new ethers.Contract(contractAddress, abi, wallet);
-
-        var numberOfTokens = ethers.utils.parseUnits('10.0',numberOfDecimals);
-        var options = { gasLimit: 1500000, gasPrice: ethers.utils.parseUnits('1.0', 'gwei') };
-        
-        var tx = await contract.transferFrom(fromAddress, targetAddress,numberOfTokens,options);
+ 
+    swapBLXMTokenETH() {
+ 
     }
-
+ 
     swapBLXMTokenBSC() {
-
+ 
     }
-
-    swapUSDTokenETH() {
-
+ 
+    async swapUSDTokenETH(amount) {
+ 
+       /* var numberOfTokens = ethers.utils.parseEther(amount);*/
+ 
+        let nounce1 = await this.wallet_ETH.getTransactionCount();
+ 
+/*         console.log(nounce1);
+ 
+        var options = {
+            nonce: nounce1
+        }
+ 
+        var txEth = await this.contract_usd_token_eth.transfer(constants.ACCOUNT_2_ADRESS, numberOfTokens, options);    
+ 
+        console.log(txEth); */
+ 
+        let nounce2 = await this.bridge_wallet_BSC.getTransactionCount();
+ 
+        numberOfTokens = ethers.utils.parseEther(amount);
+ 
+        console.log(nounce2);
+ 
+        var options2 = {
+            nonce: nounce2
+        }
+ 
+        var txBsc = await this.bridge_contract_usd_token_bsc.transfer(constants.ACCOUNT_1_ADRESS, numberOfTokens, options2);    
+ 
+        console.log(txBsc);
     }
-
+ 
     swapUSDTokenBSC() {
-
+ 
     }
 }
-
-
+ 
+var bridge = new BridgeService();
+bridge.swapUSDTokenETH('1.0');
+ 
 module.exports = BridgeService;
-
-console.log(ethers.utils.parseEther((15.2).toString()).toString());
