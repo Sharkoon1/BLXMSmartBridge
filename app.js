0e2ed79d6338fc216acae3757ae885a8076091ce
Felix Flassak <felixflassak@gmail.com>
2021-12-03 16:49:38 +0100

    Added winston logging

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+var logger = require("./logger/logger")
+const winston = require('winston');
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
-var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var apiRouter = require("./routes/api");
 var apiResponse = require("./helpers/apiResponse");
@@ -11,15 +12,22 @@ const ArbitrageService = require("./service/ArbitrageService");
 const BridgeService = require("./service/BridgeService");
 // DB connection
 
-var bridgeService = new BridgeService();
-var arbitrageService = new ArbitrageService(bridgeService);
+//var bridgeService = new BridgeService();
+//var arbitrageService = new ArbitrageService(bridgeService);
 
 var app = express();
 
-//don't show the log when it is test
-if (process.env.NODE_ENV !== "test") {
-	app.use(logger("dev"));
-}
+//different logger experessions 
+logger.error("error");
+logger.warn("warn");
+logger.info("info");
+logger.http("http");
+logger.verbose("verbose")
+logger.debug("debug")
+logger.silly("silly")
+
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
