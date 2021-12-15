d85513b3ad3cb4767eaf2cd3c1dc83f38ff5f227
Igor <rowan93@web.de>
2021-12-15 16:20:29 +0100

    Added Button design.

--- frontend/src/Buttons.js
+++ frontend/src/Buttons.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 const Buttons = () => {
 
 	const [errorMessage, setErrorMessage] = useState(null);
-	const [connButtonText, setConnButtonText] = useState("Job Running");
+	const [connButtonText, setConnButtonText] = useState("Job not running");
 
 	const startArbitrage = () => {
 
@@ -19,11 +19,25 @@ const Buttons = () => {
 	};
 
 	const startJob = () => {
+		/*
+		fetch("http://localhost:3000/api/singleArbitrage",
+			{
+				method: "post",
+			}).then(() => {
+			setConnButtonText(connButtonText === "Job Running" ? "Job not running" : "Job running");
+		})
+			.catch(error => {
+				setErrorMessage(error.message);
+			});
+		*/
+		setConnButtonText(connButtonText === "Job running" ? "Job not running" : "Job running");
 	};
-
+	const Buttonstyle = {
+		margin: "50px"
+	};
 	return (
 		<div>
-			<button onClick={startArbitrage} className="button">Run Single Arbitrage Cycle</button>
+			<button onClick={startArbitrage} className="button" style={Buttonstyle}>Run Single Arbitrage Cycle</button>
 			<button onClick={startJob} className="button">{connButtonText}</button>
 			{errorMessage}
 		</div>
