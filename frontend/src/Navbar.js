93c91762265a2d92812f6c25facd703dfc213948
Mirco <sharkoon1972@gmail.com>
2021-12-19 12:10:45 +0100

    show hide admin button mvp

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -6,13 +6,32 @@ import "./style/Navbar.css";
 export default function App() {
 
     const [show, setShow] = useState(false);
-    
+
+    function onClick() {
+        const root = document.getElementById("root");
+        const root1 = document.getElementById("root1");
+        const buttons = document.getElementById("buttons");
+
+
+        if(root.style.display === "none" || root1.style.display === "none") {
+            root.style.display = "block";
+            root1.style.display = "block";
+            buttons.style.display = "none";
+        }
+        
+        else {
+            root.style.display = "none";
+            root1.style.display = "none";
+            buttons.style.display = "block";
+        }
+    }
+
      
     return (
         <div class="topnav">
         <a href="" class="active"> <img class="image" src="../bloxmove.png"></img></a>
             <div id="myLinks" style={{ display: show ? "block" : "none" }}>
-                <a href="#admin">Admin</a>
+                <button id="adminButton" onClick={onClick}>Admin</button>
             </div>
             
             <button class="icon" onClick={() => setShow((s) => !s)}> 
