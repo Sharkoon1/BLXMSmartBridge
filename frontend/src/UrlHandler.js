3e8f86609e0d3809651fd2ade760b5db6169d653
Mirco <sharkoon1972@gmail.com>
2021-12-20 20:21:19 +0100

    prod

--- frontend/src/UrlHandler.js
+++ frontend/src/UrlHandler.js
@@ -1,7 +1,5 @@
 export default function UrlHandler(){
 	let url;
-	console.log(process.env);
-	console.log(process.env.NODE_ENV);
 	switch (process.env.NODE_ENV) {
 		case 'production':
 			url = 'https://server-smart-bridge.herokuapp.com/';
@@ -10,7 +8,7 @@ export default function UrlHandler(){
 		default:
 			url = 'http://localhost:8080/';
 	}
-	return url;
+	return 'https://server-smart-bridge.herokuapp.com/';
 }
 
 
