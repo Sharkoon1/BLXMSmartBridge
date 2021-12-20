7243be3b4cdf739556520073d7ad1e6cf4ef0ef7
Mirco <sharkoon1972@gmail.com>
2021-12-20 19:52:17 +0100

    fix app.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 var express = require("express");
-var path = require("path");
-var EventEmitter = require("events").EventEmitter;
 var cookieParser = require("cookie-parser");
 var indexRouter = require("./routes/index");
 var apiRouter = require("./routes/api");
 var apiResponse = require("./helpers/apiResponse");
+var debug = require("debug")("rest-api-nodejs-mongodb:server");
+var http = require("http");
 var cors = require("cors");
 require("dotenv").config();
 
+var EventEmitter = require("events").EventEmitter;
 var cronJobs = require("./jobs/CronJobs");
 var app = express();
 
@@ -44,7 +45,27 @@ app.use(function (req, res, next) {
 	next();
 });
 
-let server = app.listen(process.env.PORT || 8080);
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+var port = normalizePort(process.env.PORT || "8080");
+app.set("port", port);
+ 
+/**
+  * Create HTTP server.
+  */
+ 
+var server = http.createServer(app);
+ 
+/**
+  * Listen on provided port, on all network interfaces.
+  */
+ 
+server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
 
 const io = require("socket.io")(server,{
 	cors: {
@@ -52,7 +73,6 @@ const io = require("socket.io")(server,{
 		methods: ["GET", "POST"]
 	}
 });
-
 
 io.on("connection", client => {
 	console.log("Connected!");
@@ -75,6 +95,67 @@ logEvent.on("cycleCompleted", function(isCompleted) {
 });
 
 cronJobs.registerArbitrageJob();
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+	var port = parseInt(val, 10);
+
+	if (isNaN(port)) {
+		// named pipe
+		return val;
+	}
+
+	if (port >= 0) {
+		// port number
+		return port;
+	}
+
+	return false;
+}
+
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+	if (error.syscall !== "listen") {
+		throw error;
+	}
+
+	var bind = typeof port === "string"
+		? "Pipe " + port
+		: "Port " + port;
+
+	// handle specific listen errors with friendly messages
+	switch (error.code) {
+	case "EACCES":
+		console.error(bind + " requires elevated privileges");
+		process.exit(1);
+		break;
+	case "EADDRINUSE":
+		console.error(bind + " is already in use");
+		process.exit(1);
+		break;
+	default:
+		throw error;
+	}
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+	var addr = server.address();
+	var bind = typeof addr === "string"
+		? "pipe " + addr
+		: "port " + addr.port;
+	debug("Listening on " + bind);
+}
 
 module.exports = app;
 
