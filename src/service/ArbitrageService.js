0a6a9159010a33486b640a781900154cf14f830a
Mirco <sharkoon1972@gmail.com>
2021-12-21 08:50:30 +0100

    log #2

--- src/service/ArbitrageService.js
+++ src/service/ArbitrageService.js
@@ -211,7 +211,7 @@ class ArbitrageService {
 				totalAdjustmentValue = Utility.bigNumberMin(adjustmentValueUSDC, totalArbitrageUsdcBsc);
 
 				// bridge usdc from bsc to eth
-				logger.warn("Not enough USDC Liquidity in BSC. Need to bridge USDC from ETH to BSC.");
+				logger.warn("Not enough USDC Liquidity in ETH. Need to bridge USDC from BSC to ETH.");
 				await this._bridgeService.bridgeUSDTokenBscToEth(totalAdjustmentValue);
 			}
 
