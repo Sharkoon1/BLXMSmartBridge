7f974c89de0ce2434fabac4619e3815d1aa94e1e
Jan Wardenga <jan.wardenga@gmail.com>
2022-01-08 18:45:58 +0100

    Test Contract with truffle on Rinkeby

--- contracts/truffle-config.js
+++ contracts/truffle-config.js
@@ -53,13 +53,11 @@ module.exports = {
      port: 8545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
     },
-    ropsten: {
-      provider: () => new HDWalletProvider([process.env.DEPLOYER_PRIVAE_KEY], `https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_ID}`),
-      network_id: 3,       // Ropsten's id
-      gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    rinkeby: {
+      provider: () => new HDWalletProvider([process.env.PRIVATE_KEY], `https://rinkeby.infura.io/v3/cc9d34df18bf4e29aa284200acaf8911`),
+      network_id: 4,       // Ropsten's id
+      gas: 4500000,        // Ropsten has a lower block limit than mainnet
+      gasPrice: 10000000000,
     },
     // Another network with more advanced options...
     // advanced: {
