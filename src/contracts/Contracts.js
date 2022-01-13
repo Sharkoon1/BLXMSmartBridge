ffbbc311801730cb22fdfdeb6e684a3253e1a8e8
Jan Wardenga <jan.wardenga@gmail.com>
2022-01-13 19:40:46 +0100

    added signer to ArbitrageContract

--- src/contracts/Contracts.js
+++ src/contracts/Contracts.js
@@ -11,13 +11,13 @@ class Contracts  {
 		if(this._network === "BSC") {
 			this.blxmTokenContract = new TokenContract(constants.BLXM_TOKEN_ADDRESS_BSC,  signer);
 			this.usdTokenContract = new TokenContract(constants.USD_TOKEN_ADRESS_BSC, signer);
-			this.arbitrageContract = new ArbitrageContract(constants.ARBITRAGE_CONTRACT_ADDRESS_BSC);
+			this.arbitrageContract = new ArbitrageContract(constants.ARBITRAGE_CONTRACT_ADDRESS_BSC, signer);
 		}
 
 		else {
 			this.blxmTokenContract = new TokenContract(constants.BLXM_TOKEN_ADDRESS_ETH,  signer);
 			this.usdTokenContract = new TokenContract(constants.USD_TOKEN_ADRESS_ETH, signer);
-			this.arbitrageContract = new ArbitrageContract(constants.ARBITRAGE_CONTRACT_ADDRESS_ETH);
+			this.arbitrageContract = new ArbitrageContract(constants.ARBITRAGE_CONTRACT_ADDRESS_ETH, signer);
 		}
 	}
 }
