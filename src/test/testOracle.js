cd45cfdd184474ec7b45b358dcf07cd33d793031
Jan <jan.wardenga@gmail.com>
2022-01-16 22:43:52 +0100

    Implemented oracles for price query. Test pending

--- src/test/testOracle.js
+++ src/test/testOracle.js
@@ -1,23 +1,49 @@
 require("dotenv").config();
 const constants = require("../constants");
 const OracleContract = require("../contracts/OracleContract");
+const { _construct } = require("../logger/logger");
 
-const UniswapOracle = new OracleContract("ETH", constants.BLXM_TOKEN_ADDRESS_ETH, constants.USD_TOKEN_ADRESS_ETH);
-const PancakeOracle = new OracleContract("BSC", constants.BLXM_TOKEN_ADDRESS_BSC, constants.USD_TOKEN_ADRESS_BSC);
 
-UniswapOracle.getPrice().then((res)=>{
-	console.log("Price Uniswap");
-	console.log(res);
-});
-UniswapOracle.getReserves().then((res)=>{
-	console.log("Reserves Uniswap");
-	console.log(res);
-});
-PancakeOracle.getPrice().then((res)=>{
-	console.log("Price Pancakeswap");
-	console.log(res);
-});
-PancakeOracle.getReserves().then((res)=>{
-	console.log("Reserves Pancakeswap");
-	console.log(res);
-});
+class test{
+	
+constructor(){
+
+	this.UniswapOracle = new OracleContract("ETH", constants.BLXM_TOKEN_ADDRESS_ETH, constants.USD_TOKEN_ADRESS_ETH);
+	this.PancakeOracle = new OracleContract("BSC", constants.BLXM_TOKEN_ADDRESS_BSC, constants.USD_TOKEN_ADRESS_BSC);
+
+	this.poolPriceEth = null;
+	this.poolPriceBsc = null;
+	
+	this.amountStableEth = null;
+	this.amountBasicEth = null;
+
+	this.amountStableBsc = null;
+	this.amountBasicBsc = null;
+
+}
+
+async startTest(){
+
+	
+	this.poolPriceEth = await this.UniswapOracle.getPrice()
+	console.log("poolPriceEth: " + this.poolPriceEth);
+
+	this.poolPriceBsc = await this.PancakeOracle.getPrice()
+	console.log("poolPriceBsc: " + this.poolPriceBsc);
+
+	this.amountStableEth = await this.UniswapOracle.getReserves();
+	console.log("amountStableEth: " + this.amountStableEth)
+
+	this.amountBasicEth = await this.UniswapOracle.getReserves();
+	console.log("amountBasicEth: " + this.amountStableEth[1])
+
+	this.amountStableBsc = await this.PancakeOracle.getReserves();
+	console.log("amountStableBsc: " + this.amountStableBsc[0])
+
+	this.amountBasicBsc = await this.PancakeOracle.getReserves();
+	console.log("amountBasicBsc: " + this.amountStableBsc[1])
+	}
+}
+
+let testObject = new test();
+testObject.startTest();
