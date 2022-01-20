ec654cf54d06afab1b12113e4d22b3a1ac820180
Igor <rowan93@web.de>
2022-01-20 22:53:22 +0100

    Finalized the functionality required to calculate standard deviation of pool prices.

--- src/service/standardDeviationService.js
+++ src/service/standardDeviationService.js
@@ -1,22 +1,35 @@
 const mongoose = require("mongoose");
 const PoolPrice = require("../models/PoolPrice");
+const OracleContract = require("../contracts/oracleContract");
+const { ethers } = require("ethers");
 
 class StandardDeviationService {
 
-	constructor(dataBaseService) {
+	constructor(dataBaseService, queryIntervalSeconds = 10) {
 		this._databaseService = dataBaseService;
+		this._oracleUniswap = new OracleContract("ETH", "0x38d9eb07a7b8df7d86f440a4a5c4a4c1a27e1a08", "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2");
+		this._oraclePancakeSwap = new OracleContract("BSC", "0x40e51e0ec04283e300f12f6bb98da157bb22036e", "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c");
+		setInterval(this.getPoolData.bind(this), queryInterval * 1000);
 	}
 
-	averageStandardDeviation() {
-		var averageStandardDeviation = ((this.standardDeviationETH() + this.standardDeviationBSC()) / 2);
-		return averageStandardDeviation;
+	getPoolData() {
+		this._oracleUniswap.getPrice().then((res) => {
+			//console.log("Price Uniswap");
+			//console.log(+res);
+			DataBaseService.AddData({
+				PoolPrice: +ethers.utils.formatEther(res),
+				Network: "ETH",
+			}, PoolPrice);
+		});
+		this._oraclePancakeSwap.getPrice().then((res) => {
+			//console.log("Price Pancakeswap");
+			//console.log(+res);
+			DataBaseService.AddData({
+				PoolPrice: +ethers.utils.formatEther(res),
+				Network: "BSC",
+			}, PoolPrice);
+		});
 	}
-
-	// Implement random walk 
-	getStandardDeviation() {
-		return 1;
-	}
-
 
 	getSeconds(seconds) {
 		var timestamp = new Date(Date.now() - seconds * 60 * 1000);
@@ -28,13 +41,12 @@ class StandardDeviationService {
 
 
 	async standardDeviation(from, to, network) {
-
 		var priceHistory = [];
 		//get data from db
 		let EarlierConstructedObjectId = this.getSeconds(from);
-		console.log(EarlierConstructedObjectId);
+		//console.log(EarlierConstructedObjectId);
 		let LaterConstructedObjectId = this.getSeconds(to);
-		console.log(LaterConstructedObjectId);
+		//console.log(LaterConstructedObjectId);
 		let query = {
 			"_id": { $gt: EarlierConstructedObjectId, $lt: LaterConstructedObjectId },
 			"Network": network
@@ -45,7 +57,7 @@ class StandardDeviationService {
 				priceHistory.push(element.PoolPrice);
 			});
 
-			return this.CalculateStandardDeviation(priceHistory);
+			return priceHistory.length > 0 ? this.CalculateStandardDeviation(priceHistory) : 0;
 		} catch (err) {
 			console.log(err);
 			throw err;
@@ -53,22 +65,10 @@ class StandardDeviationService {
 	}
 
 
-	CalculateStandardDeviation(numbersArr) {
-
-		//calculate average 
-		let total = 0;
-		for (let key in numbersArr)
-			total += numbersArr[key];
-		let meanVal = total / numbersArr.length;
-
-
-		//calculate standard deviation
-		let SDprep = 0;
-		for (let key in numbersArr)
-			SDprep += Math.pow((parseFloat(numbersArr[key]) - meanVal), 2);
-		let SDresult = Math.sqrt(SDprep / numbersArr.length);
-
-		return SDresult;
+	CalculateStandardDeviation(array) {
+		const n = array.length;
+		const mean = array.reduce((a, b) => a + b) / n;
+		return Math.sqrt(array.map(x => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n);
 	}
 }
 
@@ -77,7 +77,9 @@ module.exports = StandardDeviationService;
 /* TEST CODE */
 //////////////////////////////////////////////////////
 /*
-let sd = new StandardDeviationService();
+require("dotenv").config();
+const DataBaseService = require("./DataBaseService");
+let sd = new StandardDeviationService(DataBaseService);
 
 function sleep(ms) {
 	return new Promise((resolve) => {
@@ -85,19 +87,14 @@ function sleep(ms) {
 	});
 }
 
+sleep(3000)
+
 let intervals = 0;
 let End = 10;
 let pauseSeconds = 1000;
 
 setInterval(() => {
-	DataBaseService.AddData({
-		PoolPrice: 1,
-		Network: "ETH",
-	}, PoolPrice);
-	intervals++;
-	if (intervals === End) {
-		clearInterval(this);
-		console.log(sd.standardDeviationETH(10, 0));
-	}
-}, pauseSeconds);
+	sd.standardDeviation(100, 0, "ETH").then(res => console.log(`Standard Deviation ETH ${res}`));
+	sd.standardDeviation(100, 0, "BSC").then(res => console.log(`Standard Deviation BSC ${res}`));
+}, 5*pauseSeconds);
 */
