8b3f72ed99aa05777c3b48b5db2cf8b28016d0d0
Jan Wardenga <jan.wardenga@gmail.com>
2022-01-23 22:15:21 +0100

    implemented workaround for big number operations

--- src/test/adjustmentTest.js
+++ src/test/adjustmentTest.js
@@ -1,48 +1,50 @@
 const { ethers } = require("ethers");
-const bn  = require('bignumber.js');
+const BigNumber  = require('bignumber.js');
 
-let two = ethers.BigNumber.from("2")
-let test = ethers.BigNumber.from("-2002019150556163623133199607229336681530")
-let minusOne = ethers.BigNumber.from("-1")
+basicCheapA = new BigNumber("200")
+stableCheapA = new BigNumber("250")
+basicExpensiveA = new BigNumber("150")
+constantCheapA = new BigNumber("50000")
+constantExpensiveA = new BigNumber("30000")
 
-basicCheap = ethers.BigNumber.from("200000000000000000000")
-stableCheap = ethers.BigNumber.from("250000000000000000000")
-basicExpensive = ethers.BigNumber.from("150000000000000000000")
-constantCheap = ethers.BigNumber.from("50000000000000000000000")
-constantExpensive = ethers.BigNumber.from("30000000000000000000000")
-
-
-ethers.BigNumber.prototype.sqrt = function() {
-    console.log(this);
-    return ethers.BigNumber.from(new bn(this.toString()).sqrt().toFixed().split('.')[0])
-  }
-
-  console.log(ethers.BigNumber.prototype);
-
+basicCheapB = 200
+stableCheapB = 250
+basicExpensiveB = 150
+constantCheapB = 50000
+constantExpensiveB = 30000
 
 function getAdjustmentValueUsdWithBigNumOperators(basicCheap, stableCheap, basicExpensive, constantCheap, constantExpensive){
 
-    let term1 = basicCheap.pow(2).mul(constantCheap).mul(constantExpensive);
-    let term2 = two.mul(basicCheap).mul(basicExpensive).mul(constantCheap).mul(constantExpensive);
-    let term3 = basicExpensive.pow(2).mul(constantCheap).mul(constantExpensive);
-    let term4 = basicCheap.pow(2).mul(stableCheap.mul(minusOne));
-    let term5 = two.mul(basicCheap).mul(basicExpensive).mul(stableCheap);
-    let term6 = basicCheap.mul(constantCheap);
-    let term7 = basicExpensive.pow(2).mul(stableCheap);
-    let term8 = basicExpensive.mul(constantCheap);
+    let term1 = basicCheap.exponentiatedBy(2).multipliedBy(constantCheap).multipliedBy(constantExpensive);
+    let term2 = basicCheap.multipliedBy(2).multipliedBy(basicExpensive).multipliedBy(constantCheap).multipliedBy(constantExpensive);
+    let term3 = basicExpensive.exponentiatedBy(2).multipliedBy(constantCheap).multipliedBy(constantExpensive);
+    let term4 = basicCheap.exponentiatedBy(2).multipliedBy(stableCheap.multipliedBy(-1));
+    let term5 = basicCheap.multipliedBy(2).multipliedBy(basicExpensive).multipliedBy(stableCheap);
+    let term6 = basicCheap.multipliedBy(constantCheap);
+    let term7 = basicExpensive.exponentiatedBy(2).multipliedBy(stableCheap);
+    let term8 = basicExpensive.multipliedBy(constantCheap);
 
-    let term9 = basicCheap.pow(2);
-    let term10 = two.mul(basicCheap).mul(basicExpensive);
-    let term11 =  basicExpensive.pow(2);
+    let term9 = basicCheap.exponentiatedBy(2);
+    let term10 = basicCheap.multipliedBy(2).multipliedBy(basicExpensive);
+    let term11 = basicExpensive.exponentiatedBy(2);    
+    
+    let adjustmentValue = ((term1.plus(term2).plus(term3).squareRoot()).plus(term4).minus(term5).plus(term6).minus(term7).plus(term8)).dividedBy((term9.plus(term10.plus(term11))));
+    
+    console.log(adjustmentValue.toString());
+
+  };
+
+function getAdjustmentValueUsd(basicCheap, stableCheap, basicExpensive, constantCheap, constantExpensive){
+
+  let adjustmentValue = (Math.sqrt(Math.pow(basicCheap, 2) * constantCheap * constantExpensive + 2 * basicCheap * basicExpensive * constantCheap * constantExpensive + 
+      Math.pow(basicExpensive, 2) * constantCheap * constantExpensive) + 
+      Math.pow(basicCheap, 2) * (-stableCheap) - 2 * basicCheap * basicExpensive * stableCheap + basicCheap * constantCheap - Math.pow(basicExpensive, 2) * 
+      stableCheap + basicExpensive * constantCheap) / (Math.pow(basicCheap,2) + 2 * basicCheap * basicExpensive + Math.pow(basicExpensive, 2));
 
 
-    let adjustmentValue = ((term1.add(term2).add(term3)).sqrt().add(term4).sub(term5).add(term6).sub(term7).add(term8)).div((term9.add(term10.add(term11))));
+    console.log(adjustmentValue)  
+}
 
-    console.log(ethers.utils.formatEther((term1.add(term2).add(term3)).sqrt().add(term4).sub(term5).add(term6).sub(term7).add(term8)));
-    console.log(ethers.utils.formatEther((term9.add(term10.add(term11)))));
+getAdjustmentValueUsdWithBigNumOperators(basicCheapA, stableCheapA, basicExpensiveA, constantCheapA, constantExpensiveA);
 
-    console.log(ethers.utils.formatEther(adjustmentValue));
-};
-
-getAdjustmentValueUsdWithBigNumOperators(basicCheap, stableCheap, basicExpensive, constantCheap, constantExpensive);
-
+getAdjustmentValueUsd(basicCheapB, stableCheapB, basicExpensiveB, constantCheapB, constantExpensiveB)
