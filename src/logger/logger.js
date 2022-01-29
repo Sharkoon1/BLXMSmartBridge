654d9b9c0211c8b48a1b05bd9e9289cfe99202f9
Mirco <sharkoon1972@gmail.com>
2022-01-29 20:53:23 +0100

    logs visual changes

--- src/logger/logger.js
+++ src/logger/logger.js
@@ -8,7 +8,7 @@ class EventLogTransport extends Transport {
 		super(opts);
 	}
 	log(info, callback) {
-		app.logEvent.emit("logMessage", `${info.message}`);
+		app.logEvent.emit("logMessage", `${new Date(info.timestamp).toLocaleTimeString() + " " + info.message}`);
 
 		callback();
 	}
@@ -17,8 +17,21 @@ class EventLogTransport extends Transport {
 
 module.exports = createLogger({
 	transports: [
-		new transports.Console(),
-		new EventLogTransport(),
+		new transports.Console({
+			format: format.combine(
+				format.timestamp({format: "MMM-DD-YYYY HH:mm:ss"}),
+				format.align(),
+				format.colorize(),
+				format.printf(info => `${info.level}: ${[info.timestamp]}: ${info.message}`),
+			)
+		}),
+		new EventLogTransport({
+			format: format.combine(
+				format.timestamp({format: "MMM-DD-YYYY HH:mm:ss"}),
+				format.align(),
+				format.printf(info => `${info.level}: ${[info.timestamp]}: ${info.message}`),
+			),
+		}),
 		new transports.MongoDB({
 			db: process.env.MONGODB_URL,
 			collection: "logs",
@@ -26,11 +39,4 @@ module.exports = createLogger({
 			decolorize: true
 		})
 	],
-
-	format:format.combine(
-		format.timestamp({format: "MMM-DD-YYYY HH:mm:ss"}),
-		format.align(),
-		format.colorize(),
-		format.printf(info => `${info.level}: ${[info.timestamp]}: ${info.message}`),
-	),
 });
